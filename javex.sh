9118f376c2853db069236a5ad14ed510f3f9e019
Sulaymane <50274801+Sulay35@users.noreply.github.com>
2022-02-05 22:29:18 +0100

    Update javex.sh

--- javex.sh
+++ javex.sh
@@ -1,8 +1,9 @@
 # parametres :
 # $1: -f : the user will give the file name in the next parameter
 # $2: [filename] : the name of the file to run the program on (convert into byte code and execute)
-# $3: -c : clear the console before execution
-# $4: -a : auto mode (don't ask to confirm the execution)
+# $3: -a : auto mode (don't ask to confirm the execution)
+# $4: -c : clear the console before execution
+
 
 if [ "$1" = "-f" ]; then
     filename=$2
@@ -12,27 +13,29 @@ fi
 
 if [ -e "$filename" ]; then
     # compile the java code
-    echo "Compilation de " $filename
-    javac $filename && echo "compilation reussie !" && 
-    [ "$4" != "-a" ] && ( echo "exec y/n" ; read awns 
-    {
-    if [ "$awns" = "y" ]; then
+    echo "Compilation de $filename ..."
+    echo -ne "[#####     (50%)]\r"
+    javac $filename && echo -ne "[##########(100%)]\r" && echo -e "\nCompilation reussie !"
+
+    if [ "$3" = "-a" ]; then 
+        echo "execution de " $filename
+        if [ "$4" = "-c" ]; then 
+            clear
+        fi
+        java $filename
+    else
+        read -p "exec y/n :" awns 
+        if [ "$awns" = "y" ]; then
             echo "execution de " $filename
-            if [ "$3" = "-c" ]; then 
+            if [ "$4" = "-c" ]; then 
                 clear
             fi
             java $filename
-    fi
-    }) || {
-        if [ "$3" = "-c" ]; then 
-            clear   
         fi
-        java $filename
-    }
+    fi
+    
 else 
     echo "Le fichier $filename n'existe pas"
 fi
 
 exit
-
-
